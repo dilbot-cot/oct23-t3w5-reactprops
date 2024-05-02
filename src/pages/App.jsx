fae163dc774af7bd41458c5ef98a9f667627e7f5
dilbot-cot <131760791+dilbot-cot@users.noreply.github.com>
2024-05-02 19:57:48 +1000

    fix: added movie cards

---
+++ src/pages/App.jsx
@@ -0,0 +1,23 @@
+import MovieList from '../components/MovieList';
+import '../stylesheets/App.css';
+
+function App() {
+
+  // 
+  // 
+
+  return (
+    <div className="App">
+      <h1>Favourite Movies</h1>
+
+      {/* <MovieList /> */}
+      <MovieList />
+      
+
+      {/*  */}
+
+    </div>
+  );
+}
+
+export default App;
